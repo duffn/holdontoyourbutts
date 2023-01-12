c7b8d1adeb5610bb7f7ff2d1a5ad19c96189ff4c
Nicholas Duffy <3457341+duffn@users.noreply.github.com>
2023-01-11 18:48:11 -0700

    Why is this Gatsby?

--- gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: `Hold on to your butts`,
-    description: `Hold on to your butts`,
-    author: `@duffn`,
-    gifUrl: `https://holdontoyourbutts.xyz/holdontoyourbutts.gif`,
-  },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
